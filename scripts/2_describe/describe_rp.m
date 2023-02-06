05811ce3dad1917080f1e36943803b81dd6921d2
Steven Errington <steven.errington@outlook.com>
2023-02-05 23:03:41 -0600

    Developed AUC/ROC analysis

--- scripts/2_describe/describe_rp.m
+++ scripts/2_describe/describe_rp.m
@@ -45,30 +45,30 @@ labels_value = [repmat({'1_Left'},length(input_sdf_left_A),1);repmat({'2_Right'}
 monkey_label = [repmat(executiveBeh.nhpSessions.monkeyNameLabel,2,1)];
 
 % Produce the figure, collapsed across all monkeys
-RP_A_figure(1,1)=gramm('x',timewins.sdf,'y',[input_sdf_left_A;input_sdf_right_A],'color',labels_value);
-RP_A_figure(1,1).stat_summary();
-RP_A_figure(1,1).axe_property('XLim',xlim_input,'YLim',ylim_input);
-RP_A_figure(1,1).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
+time_electrode_AUC(1,1)=gramm('x',timewins.sdf,'y',[input_sdf_left_A;input_sdf_right_A],'color',labels_value);
+time_electrode_AUC(1,1).stat_summary();
+time_electrode_AUC(1,1).axe_property('XLim',xlim_input,'YLim',ylim_input);
+time_electrode_AUC(1,1).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
 
-RP_A_figure(2,1)=gramm('x',timewins.sdf,'y',[input_sdf_left_A;input_sdf_right_A],'color',labels_value);
-RP_A_figure(2,1).stat_summary();
-RP_A_figure(2,1).axe_property('XLim',xlim_input,'YLim',ylim_input);
-RP_A_figure(2,1).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
-RP_A_figure(2,1).facet_grid([],monkey_label);
+time_electrode_AUC(2,1)=gramm('x',timewins.sdf,'y',[input_sdf_left_A;input_sdf_right_A],'color',labels_value);
+time_electrode_AUC(2,1).stat_summary();
+time_electrode_AUC(2,1).axe_property('XLim',xlim_input,'YLim',ylim_input);
+time_electrode_AUC(2,1).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
+time_electrode_AUC(2,1).facet_grid([],monkey_label);
 
-RP_A_figure(1,2)=gramm('x',timewins.sdf,'y',[input_sdf_left_B;input_sdf_right_B],'color',labels_value);
-RP_A_figure(1,2).stat_summary();
-RP_A_figure(1,2).axe_property('XLim',xlim_input,'YLim',ylim_input);
-RP_A_figure(1,2).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
+time_electrode_AUC(1,2)=gramm('x',timewins.sdf,'y',[input_sdf_left_B;input_sdf_right_B],'color',labels_value);
+time_electrode_AUC(1,2).stat_summary();
+time_electrode_AUC(1,2).axe_property('XLim',xlim_input,'YLim',ylim_input);
+time_electrode_AUC(1,2).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
 
-RP_A_figure(2,2)=gramm('x',timewins.sdf,'y',[input_sdf_left_B;input_sdf_right_B],'color',labels_value);
-RP_A_figure(2,2).stat_summary();
-RP_A_figure(2,2).axe_property('XLim',xlim_input,'YLim',ylim_input);
-RP_A_figure(2,2).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
-RP_A_figure(2,2).facet_grid([],monkey_label);
+time_electrode_AUC(2,2)=gramm('x',timewins.sdf,'y',[input_sdf_left_B;input_sdf_right_B],'color',labels_value);
+time_electrode_AUC(2,2).stat_summary();
+time_electrode_AUC(2,2).axe_property('XLim',xlim_input,'YLim',ylim_input);
+time_electrode_AUC(2,2).set_names('x','Time from Saccade (ms)','y','EEG (uV)');
+time_electrode_AUC(2,2).facet_grid([],monkey_label);
 
 RP_A_figure_out = figure('Renderer', 'painters', 'Position', [100 100 1500 800]);
-RP_A_figure.draw();
+time_electrode_AUC.draw();
 
 % Once we're done with a page, save it and close it.
 filename = fullfile(dirs.root,'results','pop_figure_rp.pdf');
@@ -78,17 +78,6 @@ close(RP_A_figure_out)
 
 clear RP_A_figure input_sdf_* plot_EEG_*
 
-%% Analysis: Sort trials by RT
-
-for session_i = 1:29
-    timewin = [-100:0]+1000;
-    clear signalA signalB
-    signalA = nanmean(EEG_signal.saccade{session_i,2}(executiveBeh.ttx.GO_Left{session_i},timewin),2);
-    signalB = nanmean(EEG_signal.saccade{session_i,2}(executiveBeh.ttx.GO_Right{session_i},timewin),2);
-    figure
-    ROC_data = roc_curve(signalB, signalA);
-    
-end
 
 %% Figure: Plot heatmap of trial-by-trial activity for each lateral electrode and for L/R trials
 % Session 1 and 2 are nice examples, later ones still show effect but a
@@ -172,4 +161,124 @@ print(example_session_heatmap,filename,'-dpdf') % then print it
 close(example_session_heatmap)
 
 clear session_rt left_rt* right_rt* trl_norm_erp
+
+
+%% Analysis: Windowed ROC analysis
+
+for session_i = 1:29
+    timewin = [-100:0]+1000;
+    
+    clear signal_AD*
+    signal_AD02_left = nanmean(EEG_signal.saccade{session_i,2}(executiveBeh.ttx.GO_Left{session_i},timewin),2);
+    signal_AD02_right = nanmean(EEG_signal.saccade{session_i,2}(executiveBeh.ttx.GO_Right{session_i},timewin),2);
+
+    signal_AD03_left = nanmean(EEG_signal.saccade{session_i,3}(executiveBeh.ttx.GO_Left{session_i},timewin),2);
+    signal_AD03_right = nanmean(EEG_signal.saccade{session_i,3}(executiveBeh.ttx.GO_Right{session_i},timewin),2);
+    
+    clear ROC_temp*
+    ROC_temp_AD02 = roc_curve(signal_AD02_right, signal_AD02_left);
+    ROC_temp_AD03 = roc_curve(signal_AD03_left, signal_AD03_right);
+        
+    filename_i = FileNames(session_i);
+    session = session_i;
+    monkey = executiveBeh.nhpSessions.monkeyNameLabel(session_i);
+    ROC_AD02_auc = ROC_temp_AD02.param.AROC;
+    ROC_AD02_curve = {ROC_temp_AD02.curve};
+    ROC_AD03_auc = ROC_temp_AD03.param.AROC;
+    ROC_AD03_curve = {ROC_temp_AD03.curve};
+    
+    ROC_analysis(session_i,:) = table(filename_i, session,...
+        monkey, ROC_AD02_auc, ROC_AD02_curve,...
+        ROC_AD03_auc, ROC_AD03_curve);
+    
+end
+
+
+%% Figure: Windowed ROC analysis
+clear electrode_label motor_auc plot_auc_electrode
+electrode_label = [repmat({'AD02'},29,1);repmat({'AD03'},29,1)];
+
+motor_auc = [ROC_analysis.ROC_AD02_auc; ROC_analysis.ROC_AD03_auc];
+plot_auc_electrode(1,1) = gramm('x',electrode_label,'y',motor_auc,'color',electrode_label);
+plot_auc_electrode(2,1) = gramm('x',electrode_label,'y',motor_auc,'color',electrode_label);
+
+plot_auc_electrode(1,1).stat_boxplot();
+plot_auc_electrode(1,1).geom_jitter('alpha',0.7);
+
+plot_auc_electrode(2,1).stat_boxplot();
+plot_auc_electrode(2,1).geom_jitter('alpha',0.7);
+
+plot_auc_electrode(2,1).facet_grid([],[ROC_analysis.monkey; ROC_analysis.monkey]);
+
+plot_auc_electrode(1,1).axe_property('YLim',[0.5 1]);
+plot_auc_electrode(2,1).axe_property('YLim',[0.5 1]);
+
+
+plot_auc_electrode_out = figure('Renderer', 'painters', 'Position', [100 100 600 800]);
+plot_auc_electrode.draw();
+
+
+% Once we're done with a page, save it and close it.
+filename = fullfile(dirs.root,'results','AUC_ROC_Basic.pdf');
+set(plot_auc_electrode_out,'PaperSize',[20 10]); %set the paper size to what you want
+print(plot_auc_electrode_out,filename,'-dpdf') % then print it
+close(plot_auc_electrode_out)
+
+
+%% Analysis: Temporal ROC analysis
+bin_step = 10;
+
+bin_window = -600:bin_step:200;
+
+warning off
+for session_i = 1:29
+    
+    for time_i = 1:length(bin_window)-1
+        timewin = [bin_window(time_i):bin_window(time_i+1)]+1000;
+        time(time_i) = median(timewin)-1000;
+        
+        
+        clear signal_AD*
+        signal_AD02_left = nanmean(EEG_signal.saccade{session_i,2}(executiveBeh.ttx.GO_Left{session_i},timewin),2);
+        signal_AD02_right = nanmean(EEG_signal.saccade{session_i,2}(executiveBeh.ttx.GO_Right{session_i},timewin),2);
+        
+        signal_AD03_left = nanmean(EEG_signal.saccade{session_i,3}(executiveBeh.ttx.GO_Left{session_i},timewin),2);
+        signal_AD03_right = nanmean(EEG_signal.saccade{session_i,3}(executiveBeh.ttx.GO_Right{session_i},timewin),2);
+        
+        clear ROC_temp*
+        ROC_temp_AD02 = roc_curve(signal_AD02_right, signal_AD02_left);
+        ROC_temp_AD03 = roc_curve(signal_AD03_left, signal_AD03_right);
+        
+        ROC_AD02_auc(session_i,time_i) = ROC_temp_AD02.param.AROC;
+        ROC_AD03_auc(session_i,time_i) = ROC_temp_AD03.param.AROC;
+        
+    end
+    
+end
+
+%% Figure: Temporal ROC analysis
+ROC_time_AD02 = num2cell(ROC_AD02_auc, 2);
+ROC_time_AD03 = num2cell(ROC_AD03_auc, 2);
+
+xlim_input = [-600 200];
+ylim_input = [0.0 1.0];
+timewins.sdf = time;
+
+labels_value = [repmat({'AD02'},length(ROC_time_AD02),1);repmat({'AD03'},length(ROC_time_AD03),1)];
+monkey_label = [repmat(executiveBeh.nhpSessions.monkeyNameLabel,2,1)];
+
+% Produce the figure, collapsed across all monkeys
+time_electrode_AUC(1,1)=gramm('x',timewins.sdf,'y',[ROC_time_AD02;ROC_time_AD03],'color',labels_value);
+time_electrode_AUC(1,1).stat_summary();
+time_electrode_AUC(1,1).axe_property('XLim',xlim_input,'YLim',ylim_input);
+time_electrode_AUC(1,1).set_names('x','Time from Saccade (ms)','y','AUC');
+
+time_electrode_AUC_out = figure('Renderer', 'painters', 'Position', [100 100 500 500]);
+time_electrode_AUC.draw();
+
+% Once we're done with a page, save it and close it.
+filename = fullfile(dirs.root,'results','AUC_ROC_time.pdf');
+set(time_electrode_AUC_out,'PaperSize',[20 10]); %set the paper size to what you want
+print(time_electrode_AUC_out,filename,'-dpdf') % then print it
+close(time_electrode_AUC_out)
 
